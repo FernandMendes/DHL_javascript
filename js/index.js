e9f3426f04d2aceefc2342a2629884ec2dbff9d7
fernandmendes <fernandmendes@hotmail.com>
2020-12-16 14:31:42 +0100

    1er event listenner

--- js/index.js
+++ js/index.js
@@ -4,3 +4,16 @@ function initialisationJS(prenom){
     jsload.style.backgroundColor='LIME';
 }
 initialisationJS('Jean');
+
+function formSubmited(evt) {
+    // pour garder le teste dans console log
+    evt.preventDefault();
+    console.log("Mon formulaire est submited")
+    // console.log(evt)
+    console.log(evt.target[0].name + "=" + evt.target[0].value);
+    console.log(evt.target[1].name + "=" + evt.target[1].value);
+    console.log(evt.target[2].name + "=" + evt.target[2].value);
+    console.log(evt.target[3].name + "=" + evt.target[3].value);
+
+};
+document.querySelector('form').addEventListener('submit',formSubmited)
