0be01e182136ae602d20ab98caacfc1c77c05039
fernandmendes <fernandmendes@hotmail.com>
2020-12-16 13:59:02 +0100

    1ere fonction avec argument

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
-function initialisationJS(){
+function initialisationJS(prenom){
     var jsload=document.querySelector("#jsload");
-    jsload.innerHTML='mon <span style="font-weight:900">JS</span> est chargé';
+    jsload.innerHTML='mon <span style="font-weight:900">JS</span> est chargé pour ' + prenom;
     jsload.style.backgroundColor='LIME';
 }
-initialisationJS();
+initialisationJS('Jean');
