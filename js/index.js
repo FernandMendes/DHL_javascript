26e72326cc42744ab463d51c0c7be9c86c7996e9
fernandmendes <fernandmendes@hotmail.com>
2020-12-16 13:56:49 +0100

    ere fct JS

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
-var jsload=document.querySelector("#jsload");
-jsload.innerHTML='mon <span style="font-weight:900">JS</span> est chargé';
-jsload.style.backgroundColor='LIME';
+function initialisationJS(){
+    var jsload=document.querySelector("#jsload");
+    jsload.innerHTML='mon <span style="font-weight:900">JS</span> est chargé';
+    jsload.style.backgroundColor='LIME';
+}
+initialisationJS();
